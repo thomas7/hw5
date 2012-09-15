48910ae9091f3bb5ac8af68be2c601f103723395
thomas7 <thomas7@vt.edu>
2012-09-14 23:01:11 -0400

    homework3 problem2 update

--- homework3/mult2.py
+++ homework3/mult2.py
@@ -8,29 +8,42 @@ import fileinput
 
 parser = argparse.ArgumentParser(description='Multiply some integers.')
 parser.add_argument("--ignore-blanks",  help="skips over blank lines if they are entered",  action = "store_true")
-parser.add_argument('--ignore-non-numeric',  help="skips over charactres that aren't integers", action = "store_true")
-parser.add_argument("infile", nargs='*',  type=argparse.FileType('r'))
+parser.add_argument("--ignore-non-numeric",  help="skips over charactres that aren't integers", action = "store_true")
+parser.add_argument("infile", nargs='*')
 
 args = parser.parse_args()
 
-product = 1
-argInc = 0
-
-if args.ignore-blanks:
-    ++argInc
-if args.ignore-non-numeric:
-    ++argInc
-
-for data in fileinput.input(sys.argv[1+argInc:]):
+#had to write my own function to check if entry is an integer, because isdigit() did not like single characters, like '2' for some reason
+def isInt(data):
     try:
-        if data == '\n': #for displaying the product if a blank line is entered
-            print product
-            product = 1
-        else:
+        int(data)
+        return True        
+    except ValueError:
+        return False
+
+product = 1
+
+for data in fileinput.input(args.infile):
+    if args.ignore_non_numeric:
+        if isInt(data):
             product *= int(data)
+        else:
+            continue
     
-    except ValueError as e:
-        e = "You must use only integers, other characters are not excepted\n"
-        sys.stderr.write(e)
-        sys.exit(1)
+    #use try block to catch non integer exceptions if ignore-non-numeric is not selected
+    else:
+        try:
+            if data == '\n': #for displaying the product if a blank line is entered
+                if args.ignore_blanks: #do not display product if blank line is enterd but --ignore-blanks option is used
+                    continue
+                else:
+                    print product
+                    product = 1
+            else:
+                product *= int(data)
+        
+        except ValueError as e:
+            e = "You must use only integers, other characters are not excepted\n"
+            sys.stderr.write(e)
+            sys.exit(1)
 print product
