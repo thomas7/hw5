bdfe2eaff14e60300ff1561cebf7e09c4535fb56
thomas7 <thomas7@vt.edu>
2012-11-09 21:57:12 -0500

    fixed child id issue

--- hw6_pipeline/dispatcher.cpp
+++ hw6_pipeline/dispatcher.cpp
@@ -2,6 +2,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
  
 using namespace std;
 
@@ -10,26 +11,28 @@ int main() {
     int status = 0;
     int pipefd[2];
     pid_t pid1, pid2;
-
-    if(fork() == 0) { // this is the child process
-        pid1 = getpid(); //get the pid for this child
-        close (pipefd[0]); //close the read end of the pipe
+    pid1 = fork();
+    
+    if(pid1 == 0) { // this is the child process     
         dup2 (pipefd[1], 1); //redirect output to write end of pipe
+        close (pipefd[0]); //close the read end of the pipe
         execve("./generator", NULL, NULL);
     }
-        
-    if(fork() == 0) { // this is the child process
-        pid2 = getpid(); // get the pid for this child
-        close (pipefd[1]); //close the write end of the pipe
+    
+    pid2 = fork();    
+    if(pid2 == 0) { // this is the child process
         dup2 (pipefd[0], 0); //redirect input to read end of pipe
+        close (pipefd[1]); //close the write end of the pipe
         execve("./consumer", NULL, NULL);
     }
     
+    sleep(1);
+    
     kill(pid1, SIGTERM);
-    waitpid(pid1, &status, WEXITED);
+    waitpid(pid1, &status, WSTOPPED);
     cerr << "child[" << pid1 << "] exited with status " << status << endl;
     
-    waitpid(pid2, &status, WEXITED);
+    waitpid(pid2, &status, WSTOPPED);
     cerr << "child[" << pid2 << "] exited with status " << status << endl;
         
 return 0;
